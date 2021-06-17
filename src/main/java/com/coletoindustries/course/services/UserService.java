b0498ee819153cf6aeb4cf42a0df98c3848d7f1f
Coletinn <gustavocoleto@hotmail.com>
2021-06-17 17:30:01 -0300

    Service layer, component registration

---
+++ src/main/java/com/coletoindustries/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.coletoindustries.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coletoindustries.course.entities.User;
+import com.coletoindustries.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
