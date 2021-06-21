78d3500068bf304335c2576df195cba00c4909fd
Coletinn <gustavocoleto@hotmail.com>
2021-06-21 12:59:14 -0300

    Payment, one-to-one association

--- src/main/java/com/coletoindustries/course/config/TestConfig.java
+++ src/main/java/com/coletoindustries/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.coletoindustries.course.entities.Category;
 import com.coletoindustries.course.entities.Order;
 import com.coletoindustries.course.entities.OrderItem;
+import com.coletoindustries.course.entities.Payment;
 import com.coletoindustries.course.entities.Product;
 import com.coletoindustries.course.entities.User;
 import com.coletoindustries.course.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 
